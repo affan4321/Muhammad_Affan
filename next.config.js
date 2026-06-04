6a50be8c5d0bf8aa75191c0164579ccce76899cc
Muhammad Affan <68880293+affan4321@users.noreply.github.com>
2026-06-04 06:11:42 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true
   },
-  assetPrefix: '/Muhammad_Affan',
 }
 
 module.exports = nextConfig
